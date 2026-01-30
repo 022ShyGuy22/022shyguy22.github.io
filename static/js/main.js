899e3b99e7620ee8bd9bd130015e9805663c91c8
022ShyGuy22 <raspberrypimachines@gmail.com>
2026-01-30 11:28:21 +0100

    AI Place holdery

--- static/js/main.js
+++ static/js/main.js
@@ -7,20 +7,3 @@ window.addEventListener('scroll', () => {
         header.classList.remove('header-scrolled');
     }
 });
-
-const menuToggle = document.querySelector('.menu-toggle');
-const navList = document.querySelector('nav ul');
-
-if (menuToggle && navList) {
-    menuToggle.addEventListener('click', () => {
-        const isOpen = navList.classList.toggle('show');
-        menuToggle.setAttribute('aria-expanded', isOpen);
-    });
-
-    document.addEventListener('click', (e) => {
-        if (!e.target.closest('nav')) {
-            navList.classList.remove('show');
-            menuToggle.setAttribute('aria-expanded', 'false');
-        }
-    });
-}
